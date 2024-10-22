4dae8adcdf69d015ae1adca9975a82fe63844377
ApsilonXi <volkov.volkova2000@mail.ru>
2024-10-22 16:01:42 +0300

    0

--- lab2.py
+++ lab2.py
@@ -2,155 +2,173 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
-import math
+from OpenGL.GLUT import * 
 
-# Функция для рисования сферы
-def draw_sphere(radius, slices, stacks):
-    for i in range(stacks):
-        lat0 = math.pi * (-0.5 + float(i) / stacks)  # углы широты
-        z0 = radius * math.sin(lat0)  # координата Z
-        r0 = radius * math.cos(lat0)  # радиус на широте
-        lat1 = math.pi * (-0.5 + float(i + 1) / stacks)  # следующий угол широты
-        z1 = radius * math.sin(lat1)
-        r1 = radius * math.cos(lat1)
+def resize(width, height):
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-5, 5, -5, 5, 2, 12)
+    gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0)
+    glMatrixMode(GL_MODELVIEW)
 
-        glBegin(GL_QUAD_STRIP)
-        for j in range(slices + 1):
-            lng = 2 * math.pi * float(j) / slices  # углы долготы
-            x = math.cos(lng)  # координаты X
-            y = math.sin(lng)
+def display():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glPushMatrix()
 
-            glNormal3f(x * r0, y * r0, z0)  # нормали
-            glVertex3f(x * r0, y * r0, z0)  # верхняя точка
-            glNormal3f(x * r1, y * r1, z1)
-            glVertex3f(x * r1, y * r1, z1)  # нижняя точка
-        glEnd()
+    #Куб 
+    glPushMatrix()
+    glTranslatef(0.5, 4, 0)
+    glColor3f(0, 0, 1)
+    glutSolidCube(1)
+    glPopMatrix()
 
-# Функция для рисования куба
-def draw_cube(size):
-    vertices = [
-        [size, size, size],
-        [size, size, -size],
-        [size, -size, -size],
-        [size, -size, size],
-        [-size, size, size],
-        [-size, size, -size],
-        [-size, -size, -size],
-        [-size, -size, size],
-    ]
+    #куб
+    glPushMatrix()
+    glTranslatef(0.5, 2, 0)
+    glColor3f(0, 1, 0)
+    glutSolidCube(1)
+    glPopMatrix()
+
+    # тор
+    glPushMatrix()
+    glTranslatef(0.5, 0, 0)
+    glColor3f(0.5, 1, 0)
+    glutSolidTorus(0.2, 0.5, 20, 20)
+    glPopMatrix()
+
+    #Цилиндр 
+    glPushMatrix()
+    glTranslatef(0.5, -2, 0)
+    glColor3f(1, 0, 0)
+    glutSolidCylinder(0.5, 1, 20, 20)
+    glPopMatrix()
+
+    # конус
+    glPushMatrix()
+    glTranslatef(0.5, -4, 0)
+    glColor3f(0.8, 0, 0) 
+    glutSolidCone(1, 1, 20, 20)
+    glPopMatrix()
+
+    # икосаэдр
+    glPushMatrix()
+    glTranslatef(3, 2, 0)
+    glColor3f(0.1, 1, 0.4) 
+    glutSolidIcosahedron()
+    glPopMatrix()
+
+    # октаэдр
+    glPushMatrix()
+    glTranslatef(3, 0, 0)
+    glColor3f(1, 1, 1)  
+    glutSolidOctahedron()
+    glPopMatrix()
+
+    # чайник
+    glPushMatrix()
+    glTranslatef(3, -2, 0)
+    glColor3f(0.3, 0, 0.5)
+    glutSolidTeapot(0.7)
+    glPopMatrix()
+
+    glPopMatrix()
+
     
-    edges = (
-        (0, 1), (1, 2), (2, 3), (3, 0),
-        (4, 5), (5, 6), (6, 7), (7, 4),
-        (0, 4), (1, 5), (2, 6), (3, 7)
-    )
-    
-    glBegin(GL_LINES)
-    for edge in edges:
-        for vertex in edge:
-            glVertex3fv(vertices[vertex])
-    glEnd()
-
-def draw_martian(position):
-    # Рисуем тело (сфера)
     glPushMatrix()
-    glTranslatef(position[0], position[1], position[2])
-    glColor3f(0.6, 0.2, 0.8)  # Фиолетовый цвет
-    draw_sphere(1, 20, 20)  # Рисуем тело
+
+    # каркасный куб
+    glPushMatrix()
+    glTranslatef(-2, 4, 0)
+    glColor3f(0, 0, 1)  
+    glutWireCube(1)
     glPopMatrix()
 
-    # Рисуем голову (сфера)
+    # каркасный куб
     glPushMatrix()
-    glTranslatef(position[0], position[1] + 1.5, position[2])
-    glColor3f(0.6, 0.2, 0.8)  # Фиолетовый цвет
-    draw_sphere(0.8, 20, 20)  # Рисуем голову
-    glPopMatrix()
-    
-    # Рисуем руки (цилиндры)
-    arm_length = 1.0
-    glColor3f(0.6, 0.2, 0.8)  # Фиолетовый цвет для рук
-    
-    # Левая рука
-    glPushMatrix()
-    glTranslatef(position[0] - 1, position[1] + 1, position[2])  # Положение левой руки
-    glRotatef(90, 0, 1, 0)  # Поворачиваем руку
-    gluCylinder(gluNewQuadric(), 0.2, 0.2, arm_length, 20, 20)  # Рисуем руку
+    glTranslatef(-2, 2, 0)
+    glColor3f(0, 1, 0)  
+    glutWireCube(1)
     glPopMatrix()
 
-    # Правая рука
+    # каркасный тор
     glPushMatrix()
-    glTranslatef(position[0] + 1, position[1] + 1, position[2])  # Положение правой руки
-    glRotatef(90, 0, 1, 0)  # Поворачиваем руку
-    gluCylinder(gluNewQuadric(), 0.2, 0.2, arm_length, 20, 20)  # Рисуем руку
+    glTranslatef(-2, 0, 0)
+    glColor3f(1, 0, 0.3)  
+    glutWireTorus(0.2, 0.5, 20, 20)
     glPopMatrix()
 
-    # Рисуем ноги (цилиндры)
-    leg_length = 1.0
-    
-    # Левая нога
+    # каркасный цилиндр
     glPushMatrix()
-    glTranslatef(position[0] - 0.5, position[1] - 1, position[2])  # Положение левой ноги
-    glRotatef(90, 0, 1, 0)  # Поворачиваем ногу
-    gluCylinder(gluNewQuadric(), 0.2, 0.2, leg_length, 20, 20)  # Рисуем ногу
+    glTranslatef(-2, -2, 0)
+    glColor3f(1, 0, 0)  # 
+    glutWireCylinder(0.5, 1, 20, 20)
     glPopMatrix()
 
-    # Правая нога
+    # каркасный конус
     glPushMatrix()
-    glTranslatef(position[0] + 0.5, position[1] - 1, position[2])  # Положение правой ноги
-    glRotatef(90, 0, 1, 0)  # Поворачиваем ногу
-    gluCylinder(gluNewQuadric(), 0.2, 0.2, leg_length, 20, 20)  # Рисуем ногу
+    glTranslatef(-2, -4, 0)
+    glColor3f(0, 1, 0)  
+    glutWireCone(1, 1, 20, 20)
     glPopMatrix()
 
-# Функция для установки освещения
-def set_lighting():
+    # каркасный икосаэдр
+    glPushMatrix()
+    glTranslatef(-4, 2, 0)
+    glColor3f(0, 1, 0)
+    glutWireIcosahedron()
+    glPopMatrix()
+
+    # каркасный октаэдр
+    glPushMatrix()
+    glTranslatef(-4, 0, 0)
+    glColor3f(1, 1, 1)
+    glutWireOctahedron()
+    glPopMatrix()
+
+    # каркасный чайник
+    glPushMatrix()
+    glTranslatef(-4, -2, 0)
+    glColor3f(1, 0, 0) 
+    glutWireTeapot(0.7)
+    glPopMatrix()
+
+    glPopMatrix()
+
+    pygame.display.flip()
+
+def init_lighting():
+    pos = [3, 3, 3, 1]
+    dir = [-1, -1, -1]
+
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_COLOR_MATERIAL)
     glEnable(GL_LIGHTING)
     glEnable(GL_LIGHT0)
 
-    light_position = [0, 1, 1, 0]  # Исходящая позиция света
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position)
-
-    ambient_light = [0.2, 0.2, 0.2, 1]  #Ambient light
-    diffuse_light = [1.0, 1.0, 1.0, 1]  #Diffuse light
-    specular_light = [1.0, 1.0, 1.0, 1]  #Specular light
-
-    glLightfv(GL_LIGHT0, GL_AMBIENT, ambient_light)
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse_light)
-    glLightfv(GL_LIGHT0, GL_SPECULAR, specular_light)
+    glLightfv(GL_LIGHT0, GL_POSITION, pos)
+    glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, dir)
 
 def main():
     pygame.init()
-    display = (800, 600)
-    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-    
-    glEnable(GL_DEPTH_TEST)  # Включение теста глубины
-    glMatrixMode(GL_PROJECTION)
-    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)  # Установка перспективы
-    glTranslatef(0.0, 0.0, -10)  # Перемещение камеры назад
+    screen = pygame.display.set_mode((800, 600), DOUBLEBUF | OPENGL)
+    glutInit() 
+    resize(800, 600)
+    glClearColor(0.5, 0.7, 1.0, 1.0)
+    init_lighting()
 
-    set_lighting()  # Устанавливаем освещение
-
-    while True:
+    clock = pygame.time.Clock()
+    running = True
+    while running:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
+            if event.type == QUIT:
+                running = False
+        display()
+        clock.tick(60)
+    pygame.quit()
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        
-        # Рисуем марсианина, куб и сферу
-        glPushMatrix()
-        glTranslatef(2, -1, 0)  # Позиция куба
-        draw_cube(1)  # Рисовать куб
-        glPopMatrix()
-        
-        glPushMatrix()
-        glTranslatef(0, 1, 0)  # Позиция сферы
-        draw_sphere(1, 10, 10)  # Рисовать сферу
-        glPopMatrix()
-
-        draw_martian((0, 0, -5))  # Положение марсианина
-        pygame.display.flip()
-        pygame.time.wait(10)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
+
+
+
